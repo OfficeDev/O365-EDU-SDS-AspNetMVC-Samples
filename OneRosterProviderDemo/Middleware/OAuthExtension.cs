16c2915bb5a782a8f24473878e5bc56126f71d27
Marc LaFleur <marcla@microsoft.com>
2018-11-02 14:51:39 -0400

    Updated OAuth

---
+++ OneRosterProviderDemo/Middleware/OAuthExtension.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+* See LICENSE in the project root for license information.
+*/
+
+using Microsoft.AspNetCore.Builder;
+using OneRosterProviderDemo.Middleware;
+
+namespace OneRosterProviderDemo.Middleware
+{
+    public static class OAuthExtension
+    {
+        public static IApplicationBuilder UseOauthMessageSigning(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<OAuth>();
+        }
+    }
+}
